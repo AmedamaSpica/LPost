d3aca627e4de4cc8e27723f9ccf1d077a0e6ca51
AmedamaSpica <spica96amedama@gmail.com>
2024-10-11 13:50:20 +0900

    復活のP

--- LPost/Assets/Script/pppppppppppppp.cs
+++ LPost/Assets/Script/pppppppppppppp.cs
@@ -16,22 +16,3 @@ public class pppppppppppppp : MonoBehaviour
         
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class pppppppppppppp : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
