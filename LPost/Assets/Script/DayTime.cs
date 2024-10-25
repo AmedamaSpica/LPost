4a898aa4a768aaf6fd3f649bb9a1473eaf1cc37e
AmedamaSpica <spica96amedama@gmail.com>
2024-10-25 14:49:14 +0900

    一週間をとるのに苦戦しているよ

--- LPost/Assets/Script/DayTime.cs
+++ LPost/Assets/Script/DayTime.cs
@@ -7,15 +7,24 @@ using TMPro;
 public class DayTime : MonoBehaviour
 {
 
-    DateTime dt;
+    int dtInt ;
+    DateTime dt;DateTime dt2;
     public TextMeshProUGUI DayText;
     // Start is called before the first frame update
     void Start()
     {
         dt = DateTime.Now;
+        dt += TimeSpan.FromDays(7);
+         
+        PlayerPrefs.SetString("dt", dt.ToString("yyyyMMdd"));
         //コンソールに表示
         DayText.text = PlayerPrefs.GetString("dt");
-        PlayerPrefs.SetString("dt", dt.ToString("yyyy/MM/dd HH:mm:ss"));
+
+        dtInt = int.Parse(PlayerPrefs.GetString("dt"));
+
+        Debug.Log(dtInt -1);
+        Debug.Log(DateTime.Compare(dt,dt2));
+        
     }
 
     // Update is called once per frame
