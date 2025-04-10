6f811d7593f1bef0296986a4a00f8e7a02e676ae
AmedamaSpica <spica96amedama@gmail.com>
2025-04-10 12:49:21 +0900

    うみょーん

--- LPost/Assets/Script/Amedama/TokorotenPush.cs
+++ LPost/Assets/Script/Amedama/TokorotenPush.cs
@@ -12,19 +12,20 @@ public class TokorotenPush : MonoBehaviour
     [SerializeField] private GameObject MainCamera;
     [SerializeField] private AudioClip Sound;
     [SerializeField] private Animator CameraTransition;
-    [SerializeField] private Button BackButton;
+    [SerializeField] private GameObject BackButton;
 
     AudioSource AudioSource;
+    int tokorotenPoint = GameManager.GMPoint;
     readonly ChatGPTManager GPT = new();
 
     // Start is called before the first frame update
     void Start()
     {
-        
-        
-        CameraTransition = MainCamera.GetComponent<Animator>();
-        transform.localScale = new Vector3(0,GPT.Point, 0) ;
 
+        BackButton.SetActive(false);
+        CameraTransition = MainCamera.GetComponent<Animator>();
+        //transform.localScale = new Vector3(6.065966f, GameManager.GMPoint * 0.1f, 5)  ;
+ 
     }
 
     // Update is called once per frame
@@ -37,27 +38,27 @@ public class TokorotenPush : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-
         
 
-        if(other.name == "tokoroten border" )
+
+        if (other.name == "tokoroten border" )
         {
            
             
-            if (transform.localScale.y >= 0.01f)
+            if (tokorotenPoint > 0)
             {
 
                
-                transform.localScale += new Vector3(0, 1, 0) * -0.1f;
+                //transform.localScale += new Vector3(0, 1, 0) * -0.1f;
 
                 GameObject tokorotenBlock = Instantiate(tokorotenPrefab, transform.position + Vector3.down * 1 + new Vector3(-1,0,-1) * Random.Range(-3.0f, 3.0f), Quaternion.identity);
-                
+                tokorotenPoint--;
 
             }
             else 
             {
                 Destroy(gameObject);
-                BackButton.SetEnabled(true);
+                BackButton.SetActive(true);
                 CameraTransition.SetBool("AllPushed", true);
 
             }
